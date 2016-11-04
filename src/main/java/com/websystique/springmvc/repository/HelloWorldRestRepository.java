4089ac725d9497f821a5fdde5c400f6f7ad106bd
sabatinipeter <sabatinipeter@gmail.com>
2016-11-03 20:34:52 -0400

    Add JdbcTemplate

--- src/main/java/com/websystique/springmvc/repository/HelloWorldRestRepository.java
+++ src/main/java/com/websystique/springmvc/repository/HelloWorldRestRepository.java
@@ -1,13 +1,19 @@
 package com.websystique.springmvc.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.websystique.springmvc.domain.Message;
 
 @Repository
 public class HelloWorldRestRepository {
-
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	public Message getMessageByPlayer(String player) {
+		
 		Message msg = new Message(player, "Hello " + player);
 		return msg;
 	}
