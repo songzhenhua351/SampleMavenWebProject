565d240959fc5068d869efd65e46ee9d52556c82
songzhenhua351 <24764505@qq.com>
2020-10-20 11:13:42 +0800

    Update HelloWorldRestService.java
    fix for #1 #2

--- src/main/java/com/websystique/springmvc/service/HelloWorldRestService.java
+++ src/main/java/com/websystique/springmvc/service/HelloWorldRestService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.websystique.springmvc.domain.Message;
 import com.websystique.springmvc.repository.HelloWorldRestRepository;
 
+
+//testing for ones.ai
 @Service
 public class HelloWorldRestService {
 	
